cf4a00c4b4742e429cfb49378c10328af91b208a
CarltonHui <35266152+CarltonHui@users.noreply.github.com>
2018-02-12 10:15:47 -1000

    Completed assignment #4

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -3,3 +3,30 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//The following is declaring an array
+var fruitBasket: [String]
+
+//The following is declaring a string
+var simpleString: String
+
+//Initialize an array using an Array literal
+fruitBasket = ["Apples", "Pineapples", "Strawberries", "Bananas"]
+
+print (fruitBasket)
+
+//for-in loop
+for name in fruitBasket{
+    
+//Print "I like to eat" before every name in fruitBasket
+print ("I like to eat " + name)
+    
+}
+
+//Items in the array are indexed from left to right starting with the number "0". Print out item at index 1.
+
+print (fruitBasket [1])
+
+//Print item in index "0"
+
+print (fruitBasket [0])
